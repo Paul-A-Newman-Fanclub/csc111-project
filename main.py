7603db0c3e217b626b304585297d071ff64617f9
1izardo <d.lazaro@mail.utoronto.ca>
2021-04-11 16:38:51 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+CSC111 Final Project: main.py file (see instructions in writeup for details)
+"""
+
+def bigquery_helper():
+    """
+    Queries the Ethereum BigQuery dataset according to user's input.
+    """
+    # TODO: Implement this function
+
+
+if __name__ == "__main__":
+    bigquery_helper()
