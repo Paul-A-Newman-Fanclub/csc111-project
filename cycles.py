b23768edb270009a36d37f0ced944d0c3d25f3ac
Matthew Parvaneh <58342627+m-parvaneh@users.noreply.github.com>
2021-04-16 10:58:22 -0400

    Update cycles.py

--- cycles.py
+++ cycles.py
@@ -10,9 +10,9 @@ Copyright Information
 This file is Copyright of Tobey Brizuela, Daniel Lazaro, Matthew Parvaneh, and
 Michael Umeh.
 """
-import networkx as nx
-
 from typing import Optional
+
+import networkx as nx
 
 
 def transaction_cycle(graph: nx.MultiDiGraph) -> None:
@@ -24,23 +24,26 @@ def transaction_cycle(graph: nx.MultiDiGraph) -> None:
     others, to see if any Ether that this account sent out eventually
     may have come back to it, as a means of studying the flow of currency
     amongst accounts.
+
+    Preconditions:
+      - list(graph.nodes) != []
     """
-    # Find any relevant nodes that we can recurse on:
+    # Find any relevant nodes that we can recurse on to find cycles:
     # Criteria:
     # - Must have at least 1 successor (outgoing transaction)
     # - Must have at least 1 predecessor (incoming transaction)
     accounts = []
     for node in graph.nodes():
-        num_predecessors = len([p for p in graph.predecessors(node)])
-        num_successors = len([s for s in graph.successors(node)])
+        num_predecessors = len(list(graph.predecessors(node)))
+        num_successors = len(list(graph.successors(node)))
 
         if num_successors >= 1 and num_predecessors >= 1:
             accounts.append(node)
 
     # Loop through all the possible candidate accounts for having a cycle.
     cycles = []
-    while accounts != []:
-        main_acc = accounts.pop()
+    for account in accounts:
+        main_acc = account
 
         # Now, we will check every successor of this main node (to see
         # whether or not it is involved in a cycle that ends back at
@@ -63,12 +66,24 @@ def transaction_cycle(graph: nx.MultiDiGraph) -> None:
 
     if cycles == []:
         print("Unfortunately, no cycles could be found.")
+    else:
+        for i in range(0, len(cycles)):
+            print(f"Cycle {i + 1}: {cycles[i]}")
 
 
 def _check_cycle(graph: nx.MultiDiGraph, current_account: str,
                  target_account: str, visited: set, length: int) -> Optional[list]:
     """
-    Recursive helper function for transaction_cycle
+    Recursive helper function for transaction_cycle.
+
+    Recurses on all of the successors of current_account to see if it is
+    connected back to target_account in the form of a cycle.
+
+    Returns a list of the cycle's path if it exists, None if it doesn't.
+
+    Preconditions:
+        - list(graph.nodes) != []
+        - length >= 0
 
     Sample Usage:
     >>> g = nx.MultiDiGraph()
@@ -76,8 +91,16 @@ def _check_cycle(graph: nx.MultiDiGraph, current_account: str,
     ...     g.add_node(str(n))
     >>> for n in range(1, 4):
     ...     g.add_edge(str(n), str(n + 1))
+    0
+    0
+    0
     >>> g.add_edge("4", "1")
+    0
     >>> cycle = _check_cycle(g, "1", "1", set(), 0)
+      Successor #0: 2
+        Successor #1: 3
+          Successor #2: 4
+            Successor #3: 1
     >>> cycle == ["1", "2", "3", "4", "1"]
     True
     """
@@ -85,6 +108,8 @@ def _check_cycle(graph: nx.MultiDiGraph, current_account: str,
     # we know that we've found a cycle! Only as long as the length isn't 0,
     # however, since at the very first call, the two must be equal.
     if current_account == target_account and length != 0:
+        # A cycle is only defined for length 3, but there are some
+        # transactions
         if length >= 3:
             return [current_account]
         else:
@@ -105,7 +130,6 @@ def _check_cycle(graph: nx.MultiDiGraph, current_account: str,
             # Print the current successor being checked and its length (to
             # keep track of where we are).
             print('  ' * (length + 1) + f"Successor #{length}: {successor}")
-            # print('  ' * (length + 1) + f"Cycle Length: {length}")
 
             # If the successor hasn't already been visited, we know we can check it
             # since it might lead us back to the original account.
@@ -125,4 +149,22 @@ def _check_cycle(graph: nx.MultiDiGraph, current_account: str,
 
 
 if __name__ == "__main__":
-    ...
+    # Check all doctests.
+    import doctest
+    doctest.testmod()
+
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 100,
+        'disable': ['E1136'],
+        'allowed-io': ['transaction_cycle', '_check_cycle'],
+        'extra-imports': ['networkx']
+    })
+
+    import python_ta.contracts
+    python_ta.contracts.check_all_contracts()
+
+    # Uncomment if necessary, example run of function.
+    # from build_graph import build_graph
+    # g = build_graph('balances.csv', 'transactions.csv')
+    # transaction_cycle(g)
